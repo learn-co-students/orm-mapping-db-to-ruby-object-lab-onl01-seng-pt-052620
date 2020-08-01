d64e2fac33b2ffdb86f62ddc9d01da75828a784f
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-08-01 00:42:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,19 +2,77 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    new_student = Student.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student 
     # create a new Student object given a row from the database
   end
 
   def self.all
+    sql = "SELECT * FROM students"
+
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end 
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+   
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      Student.new_from_db(row) 
+    end.first  
+
     # find the student in the database given a name
     # return a new instance of the Student class
   end
+
+  def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = '9'"
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row) 
+    end  
+  end 
   
+  def self.first_student_in_grade_10 
+    sql = "SELECT * FROM students WHERE grade = '10'"
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row) 
+    end.first   
+  end 
+  
+  def self.all_students_in_grade_X(number)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, number).map do |row|
+      Student.new_from_db(row) 
+    end  
+  end 
+  
+  
+  
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade <= '11'"
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end  
+  end 
+
+
+
+  def self.first_X_students_in_grade_10(number)
+     #binding.pry 
+    sql = "SELECT * FROM students WHERE grade = '10' LIMIT ?"
+    DB[:conn].execute(sql, number).map do |row|
+      Student.new_from_db(row)
+    end
+    
+  end 
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
